80f49e9dceab081c65e42f65219f5c45f85030b8
Anton Rudakov <anton.rudakov216@mail.ru>
2022-02-22 19:25:07 +0300

    Добавление коментариев к коду

--- game.cpp
+++ game.cpp
@@ -1,6 +1,14 @@
 #include "game.hpp"
 
+/*В программе используются Escape-код
+Перемещение курсора вверх на N строк: \033[NA
+Перемещение курсора вниз на N строк: \033[NB
+Перемещение курсора вперед на N столбцов: \033[NC
+Перемещение курсора назад на N столбцов: \033[ND
+Стереть до конца строки: \033[K*/
+
 bool checkEndGame(std::string buffMap, std::string message){
+	// Данная функция не использует шаблон вопросов, так как необходимо перед вопросом вывести результат прошлой игры
 	std::string question("Хотите сыграть ещё раз?(y/n)");
 	std::string answer[2];
 	std::string input;
@@ -56,14 +64,17 @@ bool startNewGame(){
 
 int checkWin(const std::string buffMap){
 	char symbol(' ');
+	// Проверка строк
 	if(buffMap[0] == buffMap[1] && buffMap[0] == buffMap[2] && buffMap[0] != ' ') symbol = buffMap[0];
 	if(buffMap[3] == buffMap[4] && buffMap[3] == buffMap[5] && buffMap[3] != ' ') symbol = buffMap[3];
 	if(buffMap[6] == buffMap[7] && buffMap[6] == buffMap[8] && buffMap[6] != ' ') symbol = buffMap[6];
 
+	// Проверка столбцов
 	if(buffMap[0] == buffMap[3] && buffMap[0] == buffMap[6] && buffMap[0] != ' ') symbol = buffMap[0];
 	if(buffMap[1] == buffMap[4] && buffMap[1] == buffMap[7] && buffMap[1] != ' ') symbol = buffMap[1];
 	if(buffMap[2] == buffMap[5] && buffMap[2] == buffMap[8] && buffMap[2] != ' ') symbol = buffMap[2];
 
+	// Проверка диагоналей
 	if(buffMap[0] == buffMap[4] && buffMap[0] == buffMap[8] && buffMap[0] != ' ') symbol = buffMap[0];
 	if(buffMap[2] == buffMap[4] && buffMap[2] == buffMap[6] && buffMap[2] != ' ') symbol = buffMap[2];
 
@@ -118,14 +129,14 @@ void calculateTurn(std::string* buffMap, const bool flagSymbol){
 
 void inputTurn(std::string* buffMap, const bool flagSymbol){
 	int x(0), y(0), error(-1);
-		while(true){
-			std::string errorName[3] = {
+		while(true){ // Бесконечный цикл, пока игрок не введёт корректные данные
+			std::string errorName[3] = { // Массив возможных ошибок
 				" значения полей указанны не верно\n",
 				" значения полей могут быть только в пределах от 1 до 3\n",
 				" данное поле уже занято\n"};
 			std::string move("");
 			getline(std::cin, move);
-			std::cout << (error >= 0 ? "\033[6A" : "\033[5A");
+			std::cout << (error >= 0 ? "\033[6A" : "\033[5A"); // Возврат каретки консоли в положение верхней левой ячейки игрового поля
 			printMap(*buffMap);
 			error = -1;
 			if(move.length() != 3){
@@ -149,7 +160,7 @@ void inputTurn(std::string* buffMap, const bool flagSymbol){
 				break;
 			}
 		}
-		std::cout << "\033[4A";
+		std::cout << "\033[4A"; // Возврат каретки консоли в положение верхней левой ячейки игрового поля
 		(*buffMap)[(y - 1) * 3 + (x - 1)] = (flagSymbol == 1 ? 'x' : 'o');
 }
 
@@ -172,8 +183,8 @@ bool logicPvE(){
 	std::string message;
 	switch (flagWin){
 	case 0: message = "Ничья!!!\n"; break;
-	case 1: message = (flagSymbol == 1 ? "Вы победили!!!\n" : "Вы Проиграли.\n"); break;
-	case 2: message = (flagSymbol == 2 ? "Вы победили!!!\n" : "Вы Проиграли.\n"); break;
+	case 1: message = (flagSymbol ? "Вы победили!!!\n" : "Вы Проиграли.\n"); break;
+	case 2: message = (!flagSymbol ? "Вы победили!!!\n" : "Вы Проиграли.\n"); break;
 	}
 	return checkEndGame(buffMap, message);
 }
@@ -203,12 +214,12 @@ void printIntro(){
 	std::cout << "        Управление производиться при помощи ввода двух чисел,\n";
 	std::cout << "        положение по оси 'x' и по оси 'y'\n";
 	std::cout << "        Левое нижнее поле имеет номер '1:1', правое верхнее '3:3'\n";
+	std::cout << "\033[4A"; // Возврат каретки консоли в положение верхней левой ячейки игрового поля
 }
 
 void printMap(const std::string buffMap){
 	clearScreen();
 	printIntro();
-	std::cout << "\033[4A";
 	std::cout	<< "┌─┬─┬─┐\n"
 				<< "│" << buffMap[6] << "│" << buffMap[7] << "│" << buffMap[8] << "│\n"  
 				<< "├─┼─┼─┤\n"
@@ -216,10 +227,10 @@ void printMap(const std::string buffMap){
 				<< "├─┼─┼─┤\n"
 				<< "│" << buffMap[0] << "│" << buffMap[1] << "│" << buffMap[2] << "│\n"  
 				<< "└─┴─┴─┘";
-	std::cout << "\033[2A\033[1C";
+	std::cout << "\033[2A\033[1C"; // Коррекция положения каретки консоли, чтобы она оказалась справа от игрового поля и под всем остальным текстом
 }
 
 void clearScreen(){
-	for(int i = 0; i < 7; ++i) {std::cout << "\033[K\n";}
-	std::cout << "\033[7A";
+	for(int i = 0; i < 7; ++i) {std::cout << "\033[K\n";} // Очистка всех строк, задействованных во время игры
+	std::cout << "\033[7A"; // Возврат каретки консоли в положение верхней левой ячейки игрового поля
 }
